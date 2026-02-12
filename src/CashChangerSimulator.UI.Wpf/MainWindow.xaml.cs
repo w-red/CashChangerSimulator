724ade8a8cfe02782d2dc18e083fdc337fb1358a
terashi <yterashi@gmail.com>
2026-02-12 16:49:45 +0900

    Refactor UI to MVVM architecture using R3 ReactiveCommand and ViewModels

--- src/CashChangerSimulator.UI.Wpf/MainWindow.xaml.cs
+++ src/CashChangerSimulator.UI.Wpf/MainWindow.xaml.cs
@@ -1,5 +1,5 @@
-﻿using System.Windows;
-using System.Windows.Controls;
+﻿using System;
+using System.Windows;
 using CashChangerSimulator.UI.Wpf.ViewModels;
 
 namespace CashChangerSimulator.UI.Wpf;
@@ -13,32 +13,6 @@ public partial class MainWindow : Window
         InitializeComponent();
         _viewModel = new MainViewModel();
         this.DataContext = _viewModel;
-    }
-
-    private void AddCash_Click(object sender, RoutedEventArgs e)
-    {
-        if (sender is Button btn && btn.Tag is int denomination)
-        {
-            _viewModel.AddCash(denomination, 1);
-        }
-    }
-
-    private void RemoveCash_Click(object sender, RoutedEventArgs e)
-    {
-        if (sender is Button btn && btn.Tag is int denomination)
-        {
-            // For now, removing cash is just dispensing 1 count
-            _viewModel.AddCash(denomination, -1);
-        }
-    }
-
-    private void Dispense_Click(object sender, RoutedEventArgs e)
-    {
-        if (decimal.TryParse(DispenseTargetBox.Text, out decimal amount))
-        {
-            _viewModel.DispenseCash(amount);
-            DispenseTargetBox.Clear();
-        }
     }
 
     protected override void OnClosed(EventArgs e)
