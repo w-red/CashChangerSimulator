5cc19515e4288518e7dfb7f1bd5b90b6e261c78d
terashi <yterashi@gmail.com>
2026-03-09 20:01:13 +0900

    refactor(ui): modernize BulkAmountInput auxiliary ViewModels

--- src/CashChangerSimulator.UI.Wpf/ViewModels/BulkAmountInputItemViewModel.cs
+++ src/CashChangerSimulator.UI.Wpf/ViewModels/BulkAmountInputItemViewModel.cs
@@ -4,14 +4,16 @@ using R3;
 namespace CashChangerSimulator.UI.Wpf.ViewModels;
 
 /// <summary>金種指定入出金用のアイテム ViewModel。</summary>
-/// <param name="key">金種キー。</param>
-/// <param name="name">表示名。</param>
+/// <param name="key">金種を一意に識別する <see cref="DenominationKey"/>。</param>
+/// <param name="name">表示用の名称（例：1000円札）。</param>
 public class BulkAmountInputItemViewModel(DenominationKey key, string name)
 {
     /// <summary>金種キー。</summary>
     public DenominationKey Key { get; } = key;
+
     /// <summary>表示名。</summary>
     public string Name { get; } = name;
-    /// <summary>数量。</summary>
+
+    /// <summary>入力された数量。</summary>
     public ReactiveProperty<int> Quantity { get; } = new(0);
 }
