cc87654ace0bcef629318eb67a32f4b186d562fd
terashi <yterashi@gmail.com>
2026-03-30 15:18:41 +0900

    refactor: convert UI test service extensions to standard C# 12

--- test/CashChangerSimulator.UI.Tests/Helpers/TestServiceCollectionExtensions.cs
+++ test/CashChangerSimulator.UI.Tests/Helpers/TestServiceCollectionExtensions.cs
@@ -10,54 +10,50 @@ namespace CashChangerSimulator.UI.Tests.Helpers;
 /// <summary>テスト用の IServiceCollection 拡張メソッドを提供します。</summary>
 public static class TestServiceCollectionExtensions
 {
-    extension(IServiceCollection services)
+    /// <summary>UI テストに必要な ViewModel とサービス（モックを含む）を登録します。</summary>
+    /// <param name="services">サービスコレクション。</param>
+    /// <returns>登録後のサービスコレクション。</returns>
+    public static IServiceCollection AddTestWpfUiServices(this IServiceCollection services)
     {
-        /// <summary>UI テストに必要な ViewModel とサービス（モックを含む）を登録します。</summary>
-        /// <returns>登録後のサービスコレクション。</returns>
-        public IServiceCollection AddTestWpfUiServices()
+        services.AddLogging();
+
+        // ViewModels
+        services.AddTransient<MainViewModel>();
+        services.AddTransient<InventoryViewModel>();
+        services.AddTransient<DepositViewModel>();
+        services.AddTransient<DispenseViewModel>();
+        services.AddTransient<AdvancedSimulationViewModel>();
+
+        AddSingletonIfNotRegistered<IViewModelFactory, ViewModelFactory>(services);
+        AddSingletonIfNotRegistered<IDepositOperationService, DepositOperationService>(services);
+        AddSingletonIfNotRegistered<IDispatcherService, WpfDispatcherService>(services);
+        AddSingletonIfNotRegistered<IDispenseOperationService, DispenseOperationService>(services);
+        AddSingletonIfNotRegistered<IInventoryOperationService, InventoryOperationService>(services);
+
+        AddMockIfNotRegistered<INotifyService>(services);
+        AddMockIfNotRegistered<IViewService>(services);
+        AddMockIfNotRegistered<IHistoryExportService>(services);
+        AddMockIfNotRegistered<IScriptExecutionService>(services);
+
+        return services;
+    }
+
+    private static void AddMockIfNotRegistered<T>(IServiceCollection services)
+        where T : class
+    {
+        if (!services.Any(d => d.ServiceType == typeof(T)))
         {
-            // 'services' 引数を直接使用
-            services.AddLogging();
-
-            // ViewModels
-            services.AddTransient<MainViewModel>();
-            services.AddTransient<InventoryViewModel>();
-            services.AddTransient<DepositViewModel>();
-            services.AddTransient<DispenseViewModel>();
-            services.AddTransient<AdvancedSimulationViewModel>();
-
-            // extension 内のヘルパーメソッドを呼び出し
-            services.AddSingletonIfNotRegistered<IViewModelFactory, ViewModelFactory>();
-            services.AddSingletonIfNotRegistered<IDepositOperationService, DepositOperationService>();
-            services.AddSingletonIfNotRegistered<IDispatcherService, WpfDispatcherService>();
-            services.AddSingletonIfNotRegistered<IDispenseOperationService, DispenseOperationService>();
-            services.AddSingletonIfNotRegistered<IInventoryOperationService, InventoryOperationService>();
-
-            services.AddMockIfNotRegistered<INotifyService>();
-            services.AddMockIfNotRegistered<IViewService>();
-            services.AddMockIfNotRegistered<IHistoryExportService>();
-            services.AddMockIfNotRegistered<IScriptExecutionService>();
-
-            return services;
+            services.AddSingleton(new Mock<T>().Object);
         }
+    }
 
-        private void AddMockIfNotRegistered<T>()
-            where T : class
+    private static void AddSingletonIfNotRegistered<TService, TImplementation>(IServiceCollection services)
+        where TService : class
+        where TImplementation : class, TService
+    {
+        if (!services.Any(d => d.ServiceType == typeof(TService)))
         {
-            if (!services.Any(d => d.ServiceType == typeof(T)))
-            {
-                services.AddSingleton(new Mock<T>().Object);
-            }
-        }
-
-        private void AddSingletonIfNotRegistered<TService, TImplementation>()
-            where TService : class
-            where TImplementation : class, TService
-        {
-            if (!services.Any(d => d.ServiceType == typeof(TService)))
-            {
-                services.AddSingleton<TService, TImplementation>();
-            }
+            services.AddSingleton<TService, TImplementation>();
         }
     }
 }
