a6ba5ae05774e913f8a3d425f59051fa608f2f6c
terashi <yterashi@gmail.com>
2026-03-31 20:31:21 +0900

    feat: add DepositErrorView for improved error state UI feedback

---
+++ src/CashChangerSimulator.UI.Wpf/Views/DepositErrorView.xaml.cs
@@ -0,0 +1,14 @@
+using System.Windows.Controls;
+
+namespace CashChangerSimulator.UI.Wpf.Views;
+
+/// <summary>
+/// DepositErrorView.xaml の相互作用ロジック
+/// </summary>
+internal partial class DepositErrorView : UserControl
+{
+    public DepositErrorView()
+    {
+        InitializeComponent();
+    }
+}
